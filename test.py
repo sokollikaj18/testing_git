a78d4d65758a20f4069301a0abfd9e6dda161b7e
CRS <johana.helena@hotmail.com>
2020-10-22 23:36:31 +0200

    Play my game

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-print("Hello")
-print("this was a bit complicated")
-=======
-<<<<<<< HEAD
-a="2"
 
-print("a")
-=======
-print("Hello")
+answer=input("Do you like git:  yes or no? ")
+if answer=="yes":
+    print("yeee ")
+if answer=="no":
+    print("Boooo")
 
-test1="I love Git"
->>>>>>> 2b2dab91e3b501818ca133ccd14c06b2fcd59e79
->>>>>>> a3b7aabca34e5ed6f7f9fbb8731f13bb9e4d36a9
