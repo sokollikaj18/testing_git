a430bc8e989275696d70c439e9c15bc2f901d562
julgoga404 <julgoga404@gmail.com>
2020-10-24 11:25:22 +0200

    hi

--- test.py
+++ test.py
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
-print("Hello")
 
-test1="I love Git"
-=======
-<<<<<<< HEAD
-print("Hello")
-print("this was a bit complicated")
-=======
-<<<<<<< HEAD
-a="2"
 
-print("a")
-=======
-print("Hello")
+answer=input("Do you like git:  yes or no? ")
+if answer=="yes":
+    print("yeee ")
+if answer=="no":
+    print("Boooo")
 
-test1="I love Git"
->>>>>>> 2b2dab91e3b501818ca133ccd14c06b2fcd59e79
->>>>>>> a3b7aabca34e5ed6f7f9fbb8731f13bb9e4d36a9
->>>>>>> 69ebeae0ed9dc9afd7699cfe3628ebbb187d2437
-
-print("Vazhdojm pak me ndryshimet")
-print("izaura domi")
+if  True :
+    while
